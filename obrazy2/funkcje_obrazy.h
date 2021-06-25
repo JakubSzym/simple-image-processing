97dc694c1b2b0cf3e26c35b11131a50720d3756c
JakubSzym <86427626+JakubSzym@users.noreply.github.com>
2021-06-25 17:25:12 +0200

    Update funkcje_obrazy.h

--- obrazy2/funkcje_obrazy.h
+++ obrazy2/funkcje_obrazy.h
@@ -1,9 +1,22 @@
-#define MAX 1024
-int czytaj(FILE*, int[][MAX], int*, int*, int*);
-int zapisz(FILE*, int*, int*, int*, int*);
-void wyswietl(char*);
-void negatyw(int*, int, int*, int*);
-void konturuj(int*, int*, int*);
-void proguj(int*, int, float, int*, int*);
-void rozmyj(int*, int, int*, int*);
-void histogram(int*, int, int*, int*);
+#ifndef FUNKCJE_OBRAZY_H
+#define FUNKCJE_OBRAZY_H
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "struct.h"
+//Funkcje działają na strukturach przechowujących dane o obrazach w formacie PGM lub PPM
+void negatyw(Obrazek*); //funkcja tworząca negatyw
+void konturuj(Obrazek*); //funkcja tworząca kontur obrazka
+int proguj(Obrazek*, float); //funkcja progująca obraz
+int rozmyj(Obrazek*, int); //funkcja rozmywania obrazu
+int zmiana_poziomow(Obrazek*, int, int); //funkcja zmieniająca poziom czerni i bieli
+
+/*Warunki:
+W funkcjach proguj() i rozmyj() nalezy zwrócić uwagę, czy próg i promień rozmycia zostały prawidłowo
+wprowadzone przez użytkownika. Odpowiednio 0 <= prog <= 1, type(promien) == promien && promien > 0 
+W funkcji zmiana_poziomow() nalezy sprawdzic, czy poziom bieli i czerni miesci sie w przedziale [0, l_odcieni]
+poziom bieli musi miec wieksza wartosc niz poziom czerni.
+*/
+
+#endif
