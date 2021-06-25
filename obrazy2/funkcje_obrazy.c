1d998f13a8820258b86657c7a97d919c1c781aa8
JakubSzym <86427626+JakubSzym@users.noreply.github.com>
2021-06-25 17:24:11 +0200

    Update funkcje_obrazy.c

--- obrazy2/funkcje_obrazy.c
+++ obrazy2/funkcje_obrazy.c
@@ -1,5 +1,5 @@
-#include "funkcje_obrazy2.h"
-
+#include "funkcje_obrazy.h"
+#include "constants.h"
 //Funkcja tworzaca negatyw obrazu wejsciowego
 void negatyw(Obrazek *obraz_wej){
   for(int i = 0;i < obraz_wej->wymy;i++){    
@@ -13,48 +13,80 @@ void negatyw(Obrazek *obraz_wej){
 void konturuj(Obrazek *obraz_wej){
   for(int i = 0;i < obraz_wej->wymy;i++){
     for(int j = 0;j < obraz_wej->wymx;j++){
-      *tab = abs(obraz_wej->tablica[i+1][j] - obraz-wej->tablica[i][j]) + abs(obraz_wej->tablica[i][j+1] - tablica[i][j]); //zmiana wartosci danego elementu zgodnie ze wzorem
+      if((i+1)<obraz_wej->wymy && (j+1)<obraz_wej->wymx){
+        obraz_wej->tablica[i][j] = abs(obraz_wej->tablica[i+1][j] - obraz_wej->tablica[i][j]) + abs(obraz_wej->tablica[i][j+1] - obraz_wej->tablica[i][j]); //zmiana wartosci danego elementu zgodnie ze wzorem
+      }
     }
   }
 }
 
 //funkcja progujaca
-void proguj(Obrazek *obraz_wej, float prog){
-  for(int i = 0;i < *wymy;i++){ 
-     for(int j = 0;j < *wymx;j++){
-       if(obraz_wej->tablica[i][j] > (int)(obraz_wej->odcienie * prog))   //jesli wartosc elementu jest wieksza od pewnego progu, to element przyjmuje wartosc maksymalna
-	 obraz_wej->tablica[i][j] = obraz_wej->odcienie
-       else
-	 obraz_wej->tablica[i][j] = 0                        //jesli wartosc elementu jest mniejsza od pewnego progu, to element przyjmuje wartosc 0
-
-     }
+int proguj(Obrazek *obraz_wej, float prog){
+  if(prog < 0 || prog > 1){
+    fprintf(stderr, "funkcje_obrazy.c proguj(): Error %d: prog powinien byc liczba z przedzialu [0,1]", B_NIEPRAWIDLOWAWARTOSC);
+    return B_NIEPRAWIDLOWAWARTOSC;
   }
+  for(int i = 0;i < obraz_wej->wymy;i++){ 
+    for(int j = 0;j < obraz_wej->wymx;j++){
+      if(obraz_wej->tablica[i][j] > (int)(obraz_wej->odcienie * prog))   //jesli wartosc elementu jest wieksza od pewnego progu, to element przyjmuje wartosc maksymalna
+        obraz_wej->tablica[i][j] = obraz_wej->odcienie;
+      else
+        obraz_wej->tablica[i][j] = 0;                        //jesli wartosc elementu jest mniejsza od pewnego progu, to element przyjmuje wartosc 0
+    }
+  }
+  return W_OK;
 }
 
 //funkcja rozmywajaca
-void rozmyj(Obrazek *obraz_wej, int promien){
+int rozmyj(Obrazek *obraz_wej, int promien){
+  //sprawdzenie warunkow
+  if (promien < 0){
+    fprintf(stderr, "funkcje_obrazy.c rozmyj(): Error %d: promien rozmycia to liczba naturalna dodatnia\n", B_NIEPRAWIDLOWAWARTOSC);
+    return B_NIEPRAWIDLOWAWARTOSC;
+  }
+  //wlasciwe dzialanie
   int suma;                                      //zmienna pomocnicza - suma wartosci kolejnych elementow
-  for(int i = 0;i < obraz_wej->wymy;i++){
-    for(int j = 0;j < obraz_wej->wymx;j++){
+  for(int i = 0; i < obraz_wej->wymy;i++){
+    for(int j = 0; j < obraz_wej->wymx;j++){
       suma = 0;                                  //wyzerowanie zmiennej suma po zakonczeniu operacji na danym elemencie
-      for(int k = 0;k < (2 * promien + 1);k++){
-	suma += obraz_wej->tablica[i-promien+k][j];            //dodanie do sumy k-tego elementu z sasiedztwa danego elementu
+      for(int k = 0; k < (2 * promien + 1);k++){
+	if((i-promien+k)>0 && (i-promien+k)<obraz_wej->wymy){
+	  suma += obraz_wej->tablica[i-promien+k][j];            //dodanie do sumy k-tego elementu z sasiedztwa danego elementu
+	}
       }
-      obraz_wej->tablica[i][j]= (int)(suma / (2 * promien + 1));    //wartosc wyjsciowa danego elementu
+      obraz_wej->tablica[i][j] = (int)(suma / (2 * promien + 1));    //wartosc wyjsciowa danego elementu
     }
   }
+  return W_OK;
 }
 
 //funkcja zmieniająca poziom czerni i bieli
-void zmiana_poziomow(Obrazek *obraz_wej, int biel, int czern){
+int zmiana_poziomow(Obrazek *obraz_wej, int biel, int czern){
+  //sprawdzenie warunkow
+  if(biel > czern){
+    if(biel < 0 || biel > obraz_wej->odcienie){
+      fprintf(stderr, "funkcje_obrazy.c zmiana_poziomow(): Error %d: Poziom bieli powinien byc pomiedzy 0, a liczba odcieni\n", B_NIEPRAWIDLOWAWARTOSC);
+      return B_NIEPRAWIDLOWAWARTOSC;
+    }
+    if(czern < 0 || czern > obraz_wej->odcienie){
+      fprintf(stderr, "funkcje_obrazy.c zmiana_poziomow(): Error %d: Poziom czerni powinien byc pomiedzy 0, a liczba odcieni\n", B_NIEPRAWIDLOWAWARTOSC);
+      return B_NIEPRAWIDLOWAWARTOSC;
+    }
+  }
+  else{
+    fprintf(stderr, "funkcje_obrazy.c zmiana_poziomow(): Error %d: Poziom bieli powinien byc wiekszy od poziomu czerni\n", B_NIEPRAWIDLOWAWARTOSC);
+    return B_NIEPRAWIDLOWAWARTOSC;
+  }
+  //wlasciwe dzialanie
   for(int i = 0; i < obraz_wej->wymy; i++){
     for(int j = 0; j < obraz_wej->wymx; j++){
-      if(obraz_wej->tablica[i][j] <= czern)
+      if(obraz_wej->tablica[i][j] <= czern) //do elementów o wartosci mniejszej lub równej czerni jest przypisywana wartość 0
 	obraz_wej->tablica[i][j] = 0;
-      else if(obraz_wej->tablica[i][j] > czern && obraz_wej->tablica[i][j] < biel)
-	obraz_wej->tablica[i][j] = (obraz_wej->tablica[i][j] - obraz_wej->odcienie) * (obraz_wej->odcienie/(biel - czern));
-      else if(obraz_wej->tablica[i][j] >= biel)
+      else if(obraz_wej->tablica[i][j] > czern && obraz_wej->tablica[i][j] < biel) //do elementów o wartości pomiędzy bielą a czernią jest przypisywana wartość wg wzoru
+	obraz_wej->tablica[i][j] = (obraz_wej->tablica[i][j] - czern) * (obraz_wej->odcienie/(biel - czern));
+      else if(obraz_wej->tablica[i][j] >= biel) //do elementów o wartości większej lub równej bieli jest przypisywana wartość maksymalna
 	obraz_wej->tablica[i][j] = obraz_wej->odcienie;
     }
   }
+  return W_OK;
 }
